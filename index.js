9fcf5aa078df10b9d570c5343ced0fc7ccbd4f73
elfive <elfive@elfive.cn>
2022-10-11 13:27:37 +0800

    fix not compatible with DSM7 and some code clean

--- index.js
+++ index.js
@@ -10,104 +10,115 @@ module.exports = function (homebridge) {
     homebridge.registerAccessory('homebridge-synology-surveillance-homemode', 'SSHomeMode', HttpMultiswitch);
 };
 
-function HttpMultiswitch(log, config) {
-    this.log = log;
+class HttpMultiswitch {
+    constructor(log, config) {
+        this.log = log;
 
-    this.name = config.name || 'MultiSwitch';
-    this.url = config.url;
+        this.name = config.name || 'MultiSwitch';
+        this.host = config.host;
 
-    this.username = config.username || '';
-    this.password = config.password || '';
-    this.sessionToken = "";
-}
+        this.username = config.username || '';
+        this.password = config.password || '';
+        this.sessionToken = "";
+    }
 
-HttpMultiswitch.prototype = {
-
-
-    httpRequest: function (path, callback, recursive) {
-        var _this = this;
-        request({
-                url: this.url + path + "&_sid=" + this.sessionToken,
-                method: "GET",
-                rejectUnauthorized: false,
-            },
-            function (error, response, body) {
-                var resp = JSON.parse(body);
-                if (resp.success || recursive) {
-                    callback(error, response, body);
+    async httpRequest(path) {
+        const url = this.host + path;
+        let that = this;
+        return new Promise((resolve, reject) => {
+            request(url, (error, response, body) => {
+                if (error) {
+                    that.log.error(error);
+                    reject(error);
                 }
-                else {
-                    _this.httpRequest("/webapi/auth.cgi?api=SYNO.API.Auth&method=login&version=3&account=" + _this.username + "&passwd=" + _this.password + "&session=SurveillanceStation&format=sid",
-                        function (err, resp, bod) {
-                            var r = JSON.parse(bod);
-                            if (r.success) {
-                                //OK logged in
-                                _this.sessionToken = r.data.sid;
-                                _this.log.info("Logged in.");
-                                //Retry the request
-                                _this.httpRequest(path, callback, true);
-                            }
-                            else {
-                                //Didnt work
-                                _this.log.error("Unable to login " + bod);
-                            }
-                        });
+                if (response.statusCode != 200) {
+                    that.log.error('Invalid status code <' + response.statusCode + '>');
+                    reject('Invalid status code <' + response.statusCode + '>');
                 }
+                resolve(body);
             });
-    },
-    getState: function (targetService, callback) {
-        this.httpRequest("/webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=GetInfo", function (error, response, responseBody) {
-            if (error) {
-                this.log.error('getPowerState failed: ' + error.message);
-                this.log('response: ' + response + '\nbody: ' + responseBody);
+        });
+    }
 
-                callback(error);
-            } else {
-                var resp = JSON.parse(responseBody);
-                callback(error, resp.data.on);
+    async login() {
+        try {
+            if (this.sessionToken !== "") {
+                return true;
             }
-        }.bind(this));
-    },
 
-    setPowerState: function (targetService, powerState, callback) {
-        var state = (powerState ? "off" : "on");
-        this.httpRequest("/webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=Switch&" + state + "=true", function (error, response, responseBody) {
-            if (error) {
-                this.log.error('setPowerState failed: ' + error.message);
-                this.log('response: ' + response + '\nbody: ' + responseBody);
-
-                callback(error);
+            const path = "/webapi/auth.cgi?api=SYNO.API.Auth&method=login&version=6&account=" + this.username + "&passwd=" + this.password + "&session=SurveillanceStation&format=sid";
+            const body = await this.httpRequest(path);
+            const response = JSON.parse(body);
+            if (response.success) {
+                this.log.debug(response);
+                this.sessionToken = response.data.sid;
             } else {
-                this.log.info('==> ' + (powerState ? "On" : "Off"));
-                callback();
+                this.log.error(path);
+                this.log.error(body);
             }
-        }.bind(this));
-    },
+            return response.success;
 
-    identify: function (callback) {
-        this.log('Identify me Senpai!');
-        callback();
-    },
+        } catch (e) {
+            return false;
+        }
+    }
 
-    getServices: function () {
+    async getState() {
+        try {
+            let isLoginSuccess = await this.login();
+            if (!isLoginSuccess) {
+                this.log.error(`getState login failed`);
+                return false;
+            }
+            const path = "/webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=GetInfo&_sid=" + this.sessionToken;
+            const body = await this.httpRequest(path);
+            const response = JSON.parse(body);
+            return (response.success && response.data.on ? true : false);
+        } catch (e) {
+            return false;
+        }
+    }
+
+    async setState(state) {
+        try {
+            let isLoginSuccess = await this.login();
+            if (!isLoginSuccess) {
+                this.log.error(`setState login failed`);
+                // return false;
+            }
+            const path = "/webapi/entry.cgi?api=SYNO.SurveillanceStation.HomeMode&version=1&method=Switch&on=" + (state ? "true" : "false") + "&_sid=" + this.sessionToken;
+            const body = await this.httpRequest(path);
+            this.log.debug(`calling setState(${state})`);
+            this.log.debug(`path = (${path})`);
+            // const response = JSON.parse(body);
+            // return (response.success ? true : false);
+        } catch (e) {
+            // return false;
+        }
+    }
+
+    getServices() {
         this.services = [];
+        let that = this;
 
-        var informationService = new Service.AccessoryInformation();
+        let informationService = new Service.AccessoryInformation();
         informationService
             .setCharacteristic(Characteristic.Manufacturer, 'Synology')
             .setCharacteristic(Characteristic.Model, 'Surveillance Station');
         this.services.push(informationService);
 
-
-        var switchService = new Service.Switch(this.name);
+        let switchService = new Service.Switch(this.name);
         switchService
             .getCharacteristic(Characteristic.On)
-            .on('set', this.setPowerState.bind(this, switchService))
-            .on('get', this.getState.bind(this, switchService));
+            .onGet(async () => {
+                return await that.getState();
+            })
+            .onSet(async (state) => {
+                await that.setState(state);
+            });
 
         this.services.push(switchService);
 
         return this.services;
     }
-};
-
+}
